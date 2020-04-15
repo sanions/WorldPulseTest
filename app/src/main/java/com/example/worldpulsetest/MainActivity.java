f6572f63304d7b98f815969cb411b52d81d86330
saniya karwa <skarwa@L-SB81N5FFT4-M-629.local>
2020-04-15 10:32:59 -0700

    Initial commit

---
+++ app/src/main/java/com/example/worldpulsetest/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.worldpulsetest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    public WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("http://www.worldpulse.com");
+        webView.setWebViewClient(new WebViewClient());
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+}
